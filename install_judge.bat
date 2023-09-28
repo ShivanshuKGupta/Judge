543e7aaa2e08873165e74ba2e7cbeeda91f4a5bb
Shivanshu Gupta <shivanshukgupta@gmail.com>
2023-09-28 10:56:17 +0530

    dolos is now optional.

--- install_judge.bat
+++ install_judge.bat
@@ -8,22 +8,24 @@ if %errorlevel% neq 0 (
     exit /b 1
 )
 
-echo Checking for an installation of node (npm)
-node --version >nul 2>&1
-if %errorlevel% neq 0 (
-    echo fatal: node is not installed or not in PATH.
-    echo Please install node from https://nodejs.org/ then run this script.
-    exit /b 1
-)
-
 set "current_location=%~dp0"
 setx PATH "%current_location%;%PATH%" /M
 
 echo judge has been added to the PATH environment variable.
 echo Please restart any open command prompts for the changes to take effect.
 
-echo Installing @dodona/dolos
-npm install -g @dodona/dolos
+echo Checking for an installation of node (npm)
+node --version >nul 2>&1
+if %errorlevel% neq 0 (
+    echo node is not installed or not in PATH.
+    echo Judge will not support Plagiarism check functionality.
+    echo Please install node from https://nodejs.org/ to enable plagiarism check.
+    exit /b 1
+)
+else (
+    echo Installing @dodona/dolos
+    npm install -g @dodona/dolos
+)
 
 echo Installation Complete.
 pause
